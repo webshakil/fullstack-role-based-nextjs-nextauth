e94b7aa19deda2edd5df69a808bbdc031c7c2c1a
webshakil <chumkysk@gmail.com>
2023-07-01 06:48:48 +0600

    some changes

--- next.config.js
+++ next.config.js
@@ -6,7 +6,10 @@ const nextConfig = {
   env: {
       API_URL: "http://localhost:3000",
     
-      
+      MONGODB_URL,
+      NEXTAUTH_SECRET,
+      GOOGLE_CLIENT_ID,
+      GOOGLE_CLIENT_SECRET,
       NEXTAUTH_URL:"http://localhost:3000"
   },
   images: {
