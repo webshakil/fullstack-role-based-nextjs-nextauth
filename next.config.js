fe37ae421278e32ce61486f2e6f891844627d9b2
webshakil <chumkysk@gmail.com>
2023-07-01 06:45:41 +0600

    some files changed

--- next.config.js
+++ next.config.js
@@ -5,10 +5,7 @@ const nextConfig = {
   },
   env: {
       API_URL: "http://localhost:3000",
-      MONGODB_URL: "mongodb+srv://topu:Hphp2010f@mernstack-pagination.dmkoc.mongodb.net/fullstack-role-based-nextjs-nextauth?retryWrites=true&w=majority",
-      NEXTAUTH_SECRET:"Coding with shakil",
-      GOOGLE_CLIENT_ID:"707582940430-pgttavl8uvv6cb5gb686b37p2o0mbkv7.apps.googleusercontent.com",
-      GOOGLE_CLIENT_SECRET:"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",
+    
       
       NEXTAUTH_URL:"http://localhost:3000"
   },
